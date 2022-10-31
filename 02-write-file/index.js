e0f03fc22835a3b07e9788eb630d5b6c17046b91
Raman Kandratchyk <kondrator@gmail.com>
2022-10-31 09:17:41 +0300

    refactor: delete useless comments in 02-write-file index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,27 +3,13 @@ function writeFile() {
     const fs = require('fs');
     const path = require('path');
     const { stdin, stdout } = process;
-    // const rl = require('readline');
-    // const rl = require("readline").createInterface({
-    //     input: process.stdin,
-    //     output: process.stdout
-    // });
-
-    // rl.on("SIGINT", function () {
-    //     process.emit("SIGINT");
-    // });
-
-    // process.on("SIGINT", function () {
-    //     //graceful shutdown
-    //     process.exit();
-    // });
 
     fs.open(
         path.join(__dirname, 'text.txt'),
         'w',
         (err) => {
             if (err) throw err;
-            console.log('File created');
+            // console.log('File created');
         }
     );
 
@@ -31,20 +17,9 @@ function writeFile() {
 
     stdin.on('data', data => {
 
-        // fs.writeFile(
-        //     path.join(__dirname, 'text.txt'),
-        //     `${data}`,
-        //     (err) => {
-        //         if (err) throw err;
-        //         console.log('Файл был создан');
-        //     }
-        // );
         let dataStr = data.toString();
         // stdout.write(dataStr);
-        console.log(dataStr.length);
-
-        // let buffer = Buffer.from(data, 'utf-8');
-        // console.log("buffer =", buffer);
+        // console.log(dataStr.length);
 
         if (dataStr.length === 6 && dataStr.slice(0, 4) === 'exit') {
             stdout.write('Введен exit\n');
@@ -62,13 +37,8 @@ function writeFile() {
 
     });
 
-    // rl.on('close', () => {
-    //     stdout.write('Процесс завершён. До скорого.');
-    // });
-
     process.on('SIGINT', () => {
         process.exit();
-        // stdout.write('Процесс завершён. До скорого.');
     });
 
     process.on('exit', () => {
