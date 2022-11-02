e106427cbf645d62e14ca43b01a1b7133c91e256
Raman Kandratchyk <kondrator@gmail.com>
2022-11-02 16:53:58 +0300

    fix: update 06-build-page task solution (html & css integration)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,7 +3,7 @@ async function buildPage() {
   const path = require('path');
   const fs = require('fs');
 
-  let components = new Object;
+  let templateStr;
 
   try {
     await fsProm.access(path.join(__dirname, 'project-dist'), fs.F_OK);
@@ -41,6 +41,19 @@ async function buildPage() {
 
 
   try {
+    fs.readFile(path.join(__dirname, 'template.html'), "utf8",
+      function (error, data) {
+        if (error) throw error;
+        templateStr = data;
+        // console.log('template111: ', templateStr);
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+
+  try {
     const componentsFiles = await fsProm.readdir(path.join(__dirname, 'components'), { withFileTypes: true });
     for (const file of componentsFiles) {
 
@@ -54,25 +67,63 @@ async function buildPage() {
           function (error, data) {
             if (error) throw error;
 
-            components[fileObj.name] = data;
-            // console.log(components);
+            let regStr = '{{' + fileObj.name + '}}';
+            let reg = new RegExp(regStr, 'g');
+            templateStr = templateStr.replace(reg, data);
 
-            // fs.appendFile(
-            //   path.join(__dirname, 'project-dist', 'bundle.css'),
-            //   `${data}`,
-            //   err => {
-            //     if (err) throw err;
-            //     console.log('File bundle.css was modified');
-            //   }
-            // );
+            if (templateStr.search(/{{|}}/g) === -1) {
+              fs.appendFile(
+                path.join(__dirname, 'project-dist', 'index.html'),
+                `${templateStr}`,
+                err => {
+                  if (err) throw err;
+                  console.log('File index.html was modified');
+                  // console.log(err);
+                }
+              );
+            }
 
+            console.log('templateStr was modified');
+            // console.log(templateStr);
+            console.log('****************************************************');
           }
         );
 
+      };
+
+    };
+  } catch (err) {
+    console.error(err);
+  }
+
+
+  try {
+    const sourceFiles = await fsProm.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+    for (const file of sourceFiles) {
+
+      let fileObj = path.parse(path.join(__dirname, 'styles', `${file.name}`));
+      // console.log(fileObj);
+
+      if (!file.isDirectory() && fileObj.ext === '.css') {
+        // console.log(`${fileObj.name} - ${fileObj.ext.slice(1)} - ${fileObj.root}`);
+        console.log(file);
+
+        fs.readFile(path.join(__dirname, 'styles', `${file.name}`), "utf8",
+          function (error, data) {
+            if (error) throw error;
+            fs.appendFile(
+              path.join(__dirname, 'project-dist', 'style.css'),
+              `${data}`,
+              err => {
+                if (err) throw err;
+                console.log('File style.css was modified');
+              }
+            );
+          }
+        );
+
+        console.log('******************************************');
       }
-
-
-
     }
   } catch (err) {
     console.error(err);
@@ -80,77 +131,16 @@ async function buildPage() {
 
 
   try {
-    fs.readFile(path.join(__dirname, 'template.html'), "utf8",
-      function (error, data) {
-        if (error) throw error;
-        fs.appendFile(
-          path.join(__dirname, 'project-dist', 'index.html'),
-          `${data}`,
-          err => {
-            if (err) throw err;
-            console.log('File index.html was modified');
-            // console.log(components.header);
-          }
-        );
-      }
-    );
+    const filesCopyDir = await fsProm.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true });
+
+    const sourceFiles = await fsProm.readdir(path.join(__dirname, 'assets'), { withFileTypes: true });
+    for (const file of sourceFiles) {
+      console.log(file);
+      await fsProm.copyFile(path.join(__dirname, 'assets', `${file.name}`), path.join(__dirname, 'project-dist', 'assets', `${file.name}`));
+    }
   } catch (err) {
     console.error(err);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // *******************************************************************************
-  // try {
-  //   const sourceFiles = await fsProm.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
-  //   for (const file of sourceFiles) {
-
-  //     let fileObj = path.parse(path.join(__dirname, 'styles', `${file.name}`));
-  //     // console.log(fileObj);
-
-  //     if (!file.isDirectory() && fileObj.ext === '.css') {
-  //       // console.log(`${fileObj.name} - ${fileObj.ext.slice(1)} - ${fileObj.root}`);
-  //       console.log(file);
-
-  //       fs.readFile(path.join(__dirname, 'styles', `${file.name}`), "utf8",
-  //         function (error, data) {
-  //           if (error) throw error;
-  //           fs.appendFile(
-  //             path.join(__dirname, 'project-dist', 'bundle.css'),
-  //             `${data}`,
-  //             err => {
-  //               if (err) throw err;
-  //               console.log('File bundle.css was modified');
-  //             }
-  //           );
-  //         }
-  //       );
-
-  //       console.log('******************************************');
-  //     }
-  //   }
-  // } catch (err) {
-  //   console.error(err);
-  // }
-
-
-
 
 }
 
